1c802f8e3c5fec8955c47eaf21ba3f9a6733fc5f
Mohammad-Rahim <mailmr.rahim@gmail.com>
2021-06-13 16:03:41 +0545

    content update

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -147,7 +147,7 @@ EMAIL_HOST_USER = "alertjob00@gmail.com"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = "********"
+EMAIL_HOST_PASSWORD = "**********"
 
 INTERNAL_IPS = [
     '127.0.0.1',
